8df50231ff75c77f805f54b888f1c0fd2813eb8b
gavin <yu18081588820@outlook.com>
2019-03-08 16:22:18 +0800

    'spa构建完成'

---
+++ project/src/index.js
@@ -0,0 +1,19 @@
+$(function(){
+    //监听hash句柄
+    window.onhashchange = hashFuc
+
+    // //初始化加载模块
+    hashFuc();
+    function hashFuc(){
+        switch(location.hash){
+            case '#index': $('#main').load('../moulde/index.html');break;
+            case '#loan': $('#main').load('../moulde/loan.html');break;
+            case '#lend': $('#main').load('../moulde/lend.html');break;
+            case '#information': $('#main').load('../moulde/information.html');break;
+            case '#security': $('#main').load('../moulde/security.html');break;
+            case '#myaccount': $('#main').load('../moulde/myaccount.html');break;
+            default: $('#main').load('../moulde/index.html'); break;
+        }
+    }
+})
+
