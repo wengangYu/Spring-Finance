c397342ac58058d0f1057614f0b64b6c84f2dd9d
ppkon <1249195686@qq.com>
2019-03-09 15:43:12 +0800

    登录页面前后端交互完成

---
+++ phpserver/getsession.php
@@ -0,0 +1,21 @@
+<?php
+    require("./crossdomain.php");//引入跨域文件
+    // $_SESSION 没有值是一个空的数组[] 有东西变成一个对象 {} 所有需要去判断是否登陆就使用count判断是否有长度
+//     if($_SESSION["username"]){
+//     //   用户已登录,直接返回用户名
+//     echo $_SESSION['username'];
+//    }else{
+//     //   用户未登录
+//     echo 'nologin';
+//    }
+// 使用count判断是否有username  count() 函数===.length 
+if(count($_SESSION)!=0){
+    //   用户已登录,直接返回用户名
+    echo $_SESSION['username'];
+   }else{
+        //   用户未登录
+    echo 'nologin';
+   }
+
+
+?>
---
+++ phpserver/login.php
@@ -0,0 +1,37 @@
+<?php
+// 1.接收前端发来的用户信息
+// 2.连接数据库,执行查询sql语句
+// 3.判断前端的用户信息和数据库信息是否一致
+// 成功 提示成功   失败  提示失败
+
+    // 用户名
+    $username=$_POST['username'];
+    // 密码
+    $pwd=$_POST['pwd'];
+    // 连接数据库 并修改第四个参数 此参数是自己的数据库名
+    $conn = mysqli_connect('localhost','root','root','p2p',3306) or die('Error');
+    // 测试 用户名zs  密码123
+    // $sql="SELECT * FROM user where username='zs' && pwd='123' ";
+    $sql="select * from user where username='$username' && pwd='$pwd' ";
+
+    // 执行sql语句 会返回一个查询的数据，但这个数据不能直接使用 只要不为空就表示登录成功
+    $data=mysqli_query($conn,$sql);
+    // 返回总数据条数：
+    $num=mysqli_num_rows($data);
+    // 判断返回数据条数
+    if($num !=0){
+        // 成功  跳转到页面区
+        // echo 'ok';
+        // seesion取值  保存用户名到session对象上
+        $_SESSION['username']=$username;
+        // PHP重定向浏览器页面：
+		//跳转页面
+		header("Location: http://localhost:3000/");  
+    }else{
+        // 失败 停留当前页面并刷新
+        // echo 'fail'; 
+        // ?后面叫query 查询
+        header("Location: http://localhost:3000/login.html?fail=fail");  
+    };
+    
+?>
