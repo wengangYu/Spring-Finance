9d03a776c967047ce11f0651fec130f83ade264d
ppkon <1249195686@qq.com>
2019-03-14 15:49:51 +0800

    充值

--- phpserver/reg.php
+++ phpserver/reg.php
@@ -2,10 +2,7 @@
 
 header('Content-Type: text/html;charset=utf-8');
 $conn = mysqli_connect('localhost','root','root','spring',3306) or die('Error');
-<<<<<<< HEAD
-=======
 
->>>>>>> f207e248ae2a9d79a422d1e4981f94852f5fe9fd
 
 if ($_POST){  //不要使用isset判断，$_POST数据已经存在，会返回为真    
      // 接受发过来的表单数据
